d6fce45034db16dfcb68039d298188598b68c460
ViniPeralta <83187126+ViniPeralta@users.noreply.github.com>
2023-12-14 14:32:08 +0000

    Deploying to master from @ ViniPeralta/financenow@7d6c93ce64a169763599f68ee84bef6e744c5ca7 🚀

--- src/utils/MoneyFormat.js
+++
@@ -1,7 +0,0 @@
-class MoneyFormat {
-    bigDecimalToReal(value) {
-        return `R$ ${value.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`
-    }
-}
-
-export default new MoneyFormat();
