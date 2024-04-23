05a5992d32969c40e985de5660579246b0b857f9
ViniciusPeralta <vinicius-peralta@hotmail.com>
2024-04-23 20:46:48 -0300

    US003_T02 Creating components folder

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Register from './pages/register/Register';
 import Home from './pages/home/Home';
 import Extract from './pages/extract/Extract';
 import Transactions from './pages/transactions/Transactions';
-import CustomerList from './pages/companies/list/CustomerList.js';
+import CompanyList from './pages/companies/list/CompanyList.js';
 
 import userStore from './store/UserStore.ts';
 
@@ -32,7 +32,7 @@ function App() {
           <Route path='/transactions/:name' 
           element={name !== '' ? <Transactions /> : <Navigate to='/'/>} />
           <Route path='/companies/:name' 
-          element={name !== '' ? <CustomerList /> : <Navigate to='/'/>} />
+          element={name !== '' ? <CompanyList /> : <Navigate to='/'/>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/companies/list/CompanyList.js
@@ -0,0 +1,11 @@
+import NavBar from '../../../components/navbar/NavBar.js';
+
+import styles from './CompanyList.module.css';
+
+const CompanyList = () => {
+    return (
+        <NavBar />
+    )
+}
+
+export default CompanyList
