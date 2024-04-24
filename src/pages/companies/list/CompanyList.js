3ef567e9ea91bf6ed8695290911f7c206d95703e
ViniciusPeralta <vinicius-peralta@hotmail.com>
2024-04-23 21:59:37 -0300

    US003_T02 Create company button finalized

--- src/pages/companies/list/CompanyList.js
+++ src/pages/companies/list/CompanyList.js
@@ -1,10 +1,28 @@
+import BasicButton from '../../../components/buttons/BasicButton.js';
 import NavBar from '../../../components/navbar/NavBar.js';
 
 import styles from './CompanyList.module.css';
 
 const CompanyList = () => {
+
+    const handleAddCompany = () => {
+    }
+
     return (
-        <NavBar />
+        <div>
+            <NavBar />
+            <div className={styles.grid}>
+                <div className={styles.createButton}>
+                    <BasicButton
+                        border={1}
+                        fontSize={13}
+                        color='green'
+                        text='+ Empresa'
+                        onClick={handleAddCompany}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
